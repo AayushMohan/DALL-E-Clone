e1fbcb67d0df9db8c08305fb47f10e767a543999
AayushMohan <aayushmohan1702@gmail.com>
2023-04-13 15:40:02 +0530

    Updated route.ts

---
+++ app/api/suggestion/route.ts
@@ -0,0 +1,12 @@
+export async function GET(request: Request) {
+  // Connect to our Microsoft Azure Function Endpoint
+  const response = await fetch('...', {
+    cache: 'no-cache',
+  })
+
+  const textData = await response.text();
+
+  return new Response(JSON.stringify(textData.trim()), {
+   status: 200,
+  })
+}
