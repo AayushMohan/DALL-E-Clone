8b0f5f553900ed48f64221abf9e935b5ef2a7431
AayushMohan <aayushmohan1702@gmail.com>
2023-04-13 14:07:16 +0530

    Added Header Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,22 @@
-import DropdownMenu from "./DropdownMenu";
 import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex p-4 justify-between items-center sticky top-0 bg-white z-50 border-b">
-      <Image
-        src="https://links.papareact.com/4t3"
-        alt="logo"
-        width={20}
-        height={20}
-        className="flex space-x-2 items-center cursor-pointer"
-      />
-      <div className="font-semibold cursor-pointer flex items-center hover:bg-gray-100 hover:p-2 rounded-lg">
-        <h2>DALL•E</h2>
-        <DropdownMenu />
+    <header className="p-5 flex justify-between  sticky top-0 bg-white z-50 shadow-md">
+      <div className="flex space-x-2 items-center">
+        <Image
+          src="https://links.papareact.com/4t3"
+          alt="logo"
+          width={20}
+          height={20}
+        />
+        <div>
+          <h1 className="font-bold">
+            The <span className="text-violet-500">AI</span> Image Generator
+          </h1>
+          <h2 className="text-xs">Powered by DALL·E 2 & Chat GPT</h2>
+        </div>
       </div>
-      <Image
-        src="https://avatars.githubusercontent.com/u/66319691?v=4"
-        alt="logo"
-        width={30}
-        height={30}
-        className="rounded-full cursor-pointer"
-      />
     </header>
   );
 };
